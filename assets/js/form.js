86676390dff6c3ae9f2d68d07e532890260144a3
Zoooe-Brooo <zhongwenli.zoe@gmail.com>
2024-07-01 13:23:23 +0930

    Update blog.html and form.js

--- assets/js/form.js
+++ assets/js/form.js
@@ -6,6 +6,8 @@ const contentInput = document.querySelector('#content');
 const submitBtn = document.querySelector('#submit');
 const pEl = document.querySelector('#error');
 
+// let formDataList = [];
+
 // TODO:  Create a function that handles the form submission. Grab the form data and store it in local storage, then redirect to the blog page using the redirectPage function. If the form is submitted with missing data, display an error message to the user.
 
 function handleFormSubmit(event) {
@@ -20,13 +22,16 @@ function handleFormSubmit(event) {
   if (formData.Username==='' || formData.Title==='' || formData.Content==='') {
     pEl.textContent = 'Please fill all blanks.';
   } else {
-    localStorage.setItem('form-data', JSON.stringify(formData));
-    usernameInput = '';
-    titleInput = '';
-    contentInput = '';
-    redirectPage('blog.html');
+localStorage.setItem('form-data', JSON.stringify(formData));
+
+    // formDataList.push(formData);
+    // usernameInput = '';
+    // titleInput = '';
+    // contentInput = '';
+    redirectPage('https://zoooe-brooo.github.io/module-4-challenge-personal-blog/blog.html');
   }
 }
+
   
 let redirectURL = '';
 
